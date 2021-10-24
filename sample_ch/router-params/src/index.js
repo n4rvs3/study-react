d1c9667e1bbf7d3a4675a6ca46fa23eb459eed9a
ibuki-matsumoto <n4rvs3apex@gmail.com>
2021-10-25 05:41:12 +0900

    create sample-router

---
+++ sample_ch/router-params/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import CustomerApp from "./CustomerApp";
+import './index.css';
+
+ReactDOM.render(
+  <CustomerApp />,
+  document.getElementById('root')
+);
